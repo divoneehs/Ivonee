80bdd1ef09bf69ad5c4ad845eae91db253ac3577
divoneehs <31254618+divoneehs@users.noreply.github.com>
2017-08-29 13:08:10 -0500

    Add files via upload
    Ya estan modificados los datos de la grafica y si corre el programa :)

--- temp.py
+++ temp.py
@@ -1,9 +1,9 @@
 #Python usa librerias como matplotlib con objetos predeterminados como pyplot que tienen propiedades que permiten enviar las instrucciones a la maquina de una forma resumida. Matplotlib es una libreria para crear graficas en 2 dimensiones. 
 import matplotlib.pyplot as plt
 #Construimos el objeto plt introduciendo valores que se grafican contra el numero de entrada. Por ejemplo si queremos graficar el crecimiento de un material por dia en centimetros. 
-plt.plot([1,2,3,4])
+plt.plot([7,8,8,9,9,10,10,10,11,11,14,15,15,18,18,22,22,29,30,30])
 #Con esta isntruccion colocamos un titulo al eje Y de la grafica. 
-plt.ylabel('Crecimiento del material X por dia [cm]')
+plt.ylabel('Num de hermanos')
 #Con esta isntruccion guardamos la imagen con el formato que queramos.
 plt.savefig('temp.png')
 #Con esta instruccion mostramos la figura. 
